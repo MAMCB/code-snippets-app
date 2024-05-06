7d07d856df6867944a3b3d2410cc9ce821a2760f
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-06 14:40:21 +0200

    created snippetForm component and styled it, added default style in layout.tsx

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -2,8 +2,36 @@
 
 const SnippetForm = () => {
   return (
-    <div>SnippetForm</div>
-  )
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="border rounded p-2 w-full"
+            type="text"
+            id="title"
+            name="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            className="border rounded p-2 w-full"
+            
+            id="code"
+            name="code"
+          />
+        </div>
+        <button className="rounded p-2 bg-blue-200" type="submit"> Create </button>
+    
+      </div>
+    </form>
+  );
 }
 
 export default SnippetForm
