f803c815fbefb498452ae0250d9723d876b3b99a
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-07 10:58:32 +0200

    created custom not found page for snippets detail page

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,15 @@
+
+
+const SnippetNotFound = () => {
+  return (
+    <div className="flex justify-evenly p-8">
+      <h1 className="text-2xl bold">404</h1>
+      <h1 className="text-xl bold">
+        {" "}
+        Sorry but we could not find that particular snippet
+      </h1>
+    </div>
+  );
+}
+
+export default SnippetNotFound;
