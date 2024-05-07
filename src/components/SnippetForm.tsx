d1682afaa6bcba2d7f237a269a2d84917b91d3c0
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-07 10:21:16 +0200

    created form submit server action function

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,8 +1,31 @@
-
+import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 const SnippetForm = () => {
+  async function handleSubmit(formData: FormData) {
+    //this needs to be a server action
+    "use server";
+
+    //Check the user's input and make sure they're valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    //Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log("Snippet created:", snippet);
+    //Redirect the user to the new snippet's page
+    redirect("/");
+
+  }
+
   return (
-    <form>
+    <form action={handleSubmit}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
