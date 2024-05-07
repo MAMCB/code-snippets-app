cb39117373dee624d8b640908f0ec82643bbe95c
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-05-07 11:04:01 +0200

    Created custom loading page for snippets details

---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,9 @@
+
+
+const SnippetDetailsLoading = () => {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default SnippetDetailsLoading;
--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -5,7 +5,7 @@ const SnippetNotFound = () => {
     <div className="flex justify-evenly p-8">
       <h1 className="text-2xl bold">404</h1>
       <h1 className="text-xl bold">
-        {" "}
+       
         Sorry but we could not find that particular snippet
       </h1>
     </div>
